c88eaa2afd00a3461ad749311c43fe9db98e3b40
gwenvael.l <glaskar@hipay.com>
2021-07-08 15:40:18 +0200

    add a bpoilerplate

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer} from 'apollo-server-express';
+
+import schema from './schema';
+import resolvers from './resolvers';
+import models from './models/models';
+
+const app = express();
+
+app.use(cors(
+	{
+		origin: "*"
+	}
+))
+
+
+const server = new ApolloServer({
+	typeDefs: schema, //type définition
+	resolvers: resolvers, //used for return data from fields from the schema
+	context: {
+		models: models,
+		me: models.users[1],
+	  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000}, () => {
+	console.log('Apollo server run on that temrinal on http://localhost:8000/graphql')
+
+})
